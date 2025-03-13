90e8c6540a06273bf15a7325ac46caafc56f7fc3
tony <tonhousto@gmail.com>
2025-03-13 07:33:45 -0400

    a

--- dist/index.js
+++ dist/index.js
@@ -1,1 +1,3 @@
+export * from "./src/ctx.jsx";
+export * from "./src/chat-ctx.jsx";
 //# sourceMappingURL=index.js.map
--- types/index.d.ts
+++ types/index.d.ts
@@ -1,4 +1,6 @@
 import { Libp2pTypeC } from "libp2p-ts";
+export * from "./src/ctx.tsx";
+export * from "./src/chat-ctx.tsx";
 declare global {
     interface Window {
         libp2p: Libp2pTypeC | undefined;
